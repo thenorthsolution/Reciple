ca7bfd7d1d0dca41da1e19d4c398811dc90cd0f5
GhexterCortes <ghextershumies@gmail.com>
2022-04-18 10:27:12 +0800

    fix incomplete export

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './reciple/classes/builders/MessageCommandBuilder';
 export * from './reciple/classes/builders/InteractionCommandBuilder';
 export * from './reciple/commandPermissions';
 export * from './reciple/flags';
+export * from './reciple/isIgnoredChannel';
 export * from './reciple/logger';
 export * from './reciple/modules';
 export * from './reciple/registerInteractionCommands';
