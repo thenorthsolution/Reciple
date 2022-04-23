83b87e528849bc3e6edfc2f21714685cd12628cb
GhexterCortes <ghextershumies@gmail.com>
2022-04-23 19:20:41 +0800

    debug mode flag to override config value

--- src/reciple/flags.ts
+++ src/reciple/flags.ts
@@ -2,6 +2,7 @@ import { program } from 'commander';
 
 export const flags = program
         .option('-t, --token <token>', 'Replace used bot token')
+        .option('-D, --debugmode', 'Enabled debug mode')
         .parse().opts();
 
 export const token = flags.token;
--- src/reciple/logger.ts
+++ src/reciple/logger.ts
@@ -1,9 +1,10 @@
 import { Logger } from 'fallout-utility';
+import { flags } from './flags';
 
 export function logger (stringifyJSON: boolean, debugmode: boolean = false) {
     return new Logger("Main", {
         addPrefixToEveryJsonNewLines: stringifyJSON,
         stringifyJSON: stringifyJSON,
-        setDebugging: debugmode
+        setDebugging: flags.debugmode as boolean|undefined || debugmode
     });
 }
