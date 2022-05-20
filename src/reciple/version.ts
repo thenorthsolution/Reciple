491b97b26008a85c578253c627b5d6f6b8e68920
GhexterCortes <ghextershumies@gmail.com>
2022-05-20 19:55:53 +0800

    feat(version.ts): Version checking functions

--- src/reciple/version.ts
+++ src/reciple/version.ts
@@ -1,1 +1,38 @@
-export const version = require('../../package.json').version;
+import { isNumber } from 'fallout-utility';
+
+export const version = require('../../package.json').version as string;
+export function validVersion(ver: string) {
+    if (!ver || typeof ver !== 'string' || ver.split('.').length < 3) return false;
+
+    const [major, minor, patch] = ver.split('.');
+    if (!isNumber(major) || !isNumber(minor) || !isNumber(patch)) return false;
+
+    return true;
+}
+
+export function parseVersion(ver: string) {
+    if (!validVersion(ver)) throw new TypeError(`Invalid version: ${ver}`);
+
+    const [major, minor, patch] = ver.split('.');
+    return { major: parseInt(major), minor: parseInt(minor), patch: parseInt(patch) };
+}
+
+export function isSupportedVersion(ver: string, supportedVersion?: string) {
+    supportedVersion = supportedVersion || version;
+
+    if (!validVersion(ver)) throw new TypeError(`Invalid version: ${ver}`);
+    if (!validVersion(supportedVersion)) throw new TypeError(`Invalid supported version: ${supportedVersion}`);
+
+    const { major, minor, patch } = parseVersion(ver);
+    const { major: supportedMajor, minor: supportedMinor, patch: supportedPatch } = parseVersion(supportedVersion);
+
+    if (major !== supportedMajor) return false;
+
+    if (minor > supportedMinor) return true;
+    if (minor < supportedMinor) return false;
+
+    if (patch > supportedPatch) return true;
+    if (patch < supportedPatch) return false;
+
+    return true;
+}
