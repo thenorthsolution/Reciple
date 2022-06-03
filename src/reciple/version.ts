ac6d987ed0dd3266c3c5333ec77d3584fdc2768f
GhexterCortes <ghextershumies@gmail.com>
2022-06-03 11:18:16 +0800

    use package sermver

--- src/reciple/version.ts
+++ src/reciple/version.ts
@@ -1,34 +1,22 @@
-import { isNumber } from 'fallout-utility';
+import semver from 'semver';
 
 export const version = require('../../package.json').version as string;
 export function validVersion(ver: string) {
-    if (!ver || typeof ver !== 'string' || ver.split('.').length < 3) return false;
-
-    const [major, minor, patch] = ver.split('.');
-    if (!isNumber(major) || !isNumber(minor) || !isNumber(patch)) return false;
-
-    return true;
+    return semver.valid(semver.coerce(ver)) !== null;
 }
 
 export function parseVersion(ver: string) {
     if (!validVersion(ver)) throw new TypeError(`Invalid version: ${ver}`);
 
-    const [major, minor, patch] = ver.split('.');
+    const [major, minor, patch] = `${semver.coerce(ver)}`?.split('.') ?? [];
     return { major: parseInt(major), minor: parseInt(minor), patch: parseInt(patch) };
 }
 
-export function isSupportedVersion(ver: string, supportedVersion?: string) {
+export function isSupportedVersion(versionRange: string, supportedVersion?: string) {
     supportedVersion = supportedVersion || version;
 
-    if (!validVersion(ver)) throw new TypeError(`Invalid version: ${ver}`);
+    if (!validVersion(versionRange)) throw new TypeError(`Invalid version: ${versionRange}`);
     if (!validVersion(supportedVersion)) throw new TypeError(`Invalid supported version: ${supportedVersion}`);
 
-    const { major, minor, patch } = parseVersion(ver);
-    const { major: supportedMajor, minor: supportedMinor, patch: supportedPatch } = parseVersion(supportedVersion);
-
-    if (major !== supportedMajor) return false;
-    if (minor !== supportedMinor) return false;
-    if (patch > supportedPatch) return false;
-
-    return true;
+    return semver.satisfies(supportedVersion, versionRange);
 }
