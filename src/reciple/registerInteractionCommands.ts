d33f60c330f2d01939b2518caa5a3ce7129d8e8d
GhexterCortes <ghextershumies@gmail.com>
2022-06-09 13:39:49 +0800

    short refactor to register commands ig

--- src/reciple/registerInteractionCommands.ts
+++ src/reciple/registerInteractionCommands.ts
@@ -17,24 +17,25 @@ export type commandBuilders = InteractionCommandBuilder|ContextMenuCommandBuilde
 
 export async function registerInteractionCommands(client: RecipleClient, cmds?: (commandBuilders|ApplicationCommandDataResolvable)[], overwriteGuilds?: GuildResolvable|GuildResolvable[]): Promise<void> {
     let commands = Object.values(cmds ?? client.commands.INTERACTION_COMMANDS).map(c => {
-        if (typeof (c as InteractionCommandBuilder).toJSON !== 'undefined') {
-            const cmd: InteractionCommandBuilder = c as InteractionCommandBuilder;
+        if (typeof (c as InteractionCommandBuilder).toJSON == 'undefined') return c as ApplicationCommandDataResolvable;
+        
+        const cmd: InteractionCommandBuilder = c as InteractionCommandBuilder;
 
-            if (cmd?.builder === 'INTERACTION_COMMAND' && client.config?.commands.interactionCommand.setRequiredPermissions) {
-                const permissions = (client.config?.permissions?.interactionCommands.enabled ?
+        if (cmd?.builder === 'INTERACTION_COMMAND' && client.config?.commands.interactionCommand.setRequiredPermissions) {
+            const permissions = (
+                    client.config?.permissions?.interactionCommands.enabled ?
                     client.config?.permissions?.interactionCommands.commands.find(cmd_ => cmd_.command.toLowerCase() === cmd.name.toLowerCase())?.permissions :
-                    undefined) ?? cmd.requiredPermissions;
+                    undefined
+                ) ?? cmd.requiredPermissions;
 
-                cmd.setRequiredPermissions(permissions as PermissionString[]);
-                client.commands.INTERACTION_COMMANDS[cmd.name] = cmd;
+            cmd.setRequiredPermissions(permissions as PermissionString[]);
+            client.commands.INTERACTION_COMMANDS[cmd.name] = cmd;
 
-                client.logger.debug(`Set required permissions for ${cmd.name} to ${permissions.join(', ')}`);
-                return cmd.toJSON() as ApplicationCommandDataResolvable;
-            }
-
-            return (c as InteractionCommandBuilder).toJSON() as ApplicationCommandDataResolvable;
+            client.logger.debug(`Set required permissions for ${cmd.name} to ${permissions.join(', ')}`);
+            return cmd.toJSON() as ApplicationCommandDataResolvable;
         }
-        return c as ApplicationCommandDataResolvable;
+
+        return (c as InteractionCommandBuilder).toJSON() as ApplicationCommandDataResolvable;
     }) ?? [];
 
     if (!commands || !commands.length) {
