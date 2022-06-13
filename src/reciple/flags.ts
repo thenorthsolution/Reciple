2c2d38538c4d299d2085f0f1b9376d4d0a51f852
GhexterCortes <ghextershumies@gmail.com>
2022-06-13 15:17:01 +0800

    Update flags.ts

--- src/reciple/flags.ts
+++ src/reciple/flags.ts
@@ -3,6 +3,7 @@ import { program } from 'commander';
 export const flags = program
         .option('-t, --token <token>', 'Replace used bot token')
         .option('-D, --debugmode', 'Enabled debug mode')
+        .option('-v, --version', 'Display version')
         .parse().opts();
 
 export const token = flags.token;
