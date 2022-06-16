a132f41f9e102ec364e3b2b588983f004c24679b
GhexterCortes <ghextershumies@gmail.com>
2022-06-16 22:06:31 +0800

    register interaction commands synchronously

--- src/reciple/registerInteractionCommands.ts
+++ src/reciple/registerInteractionCommands.ts
@@ -47,15 +47,18 @@ export async function registerInteractionCommands(client: RecipleClient, cmds?: 
     const guilds: (string|undefined)[] = typeof configGuilds === 'object' ? (configGuilds as string[]) : [configGuilds];
 
     if (!guilds || !guilds?.length) {
-        await client.application?.commands.set(commands)?.catch(e => client.logger.error(e));
-        client.logger.warn('No guilds were specified for interaction commands. Registered commands for all guilds.');
+        client.application?.commands.set(commands).then(() => {
+            client.logger.warn('No guilds were specified for interaction commands. Registered commands for all guilds.');
+        }).catch(e => client.logger.error(e));
     } else {        
         client.logger.warn(`Registering ${commands.length} interaction commands for ${guilds.length} guild(s).`);
+        
         for (const guild of guilds) {
             if (!guild) continue;
 
-            await client.application?.commands.set(commands, guild)?.catch(e => client.logger.error(e));
-            client.logger.warn(`Registered ${commands.length} interaction commands for ${guild}.`);
+            client.application?.commands.set(commands, guild).then(() => {
+                client.logger.warn(`Registered ${commands.length} interaction commands for ${guild}.`);
+            }).catch(e => client.logger.error(e));
         }
     }
 }
