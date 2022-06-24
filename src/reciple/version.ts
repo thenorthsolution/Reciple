c25179f670f85b413765c99d94d1b62940ad5ae4
GhexterCortes <ghextershumies@gmail.com>
2022-06-25 02:05:51 +0800

    change `validVersion` function name to more consistent name

--- src/reciple/version.ts
+++ src/reciple/version.ts
@@ -1,12 +1,12 @@
 import semver from 'semver';
 
 export const version = require('../../package.json').version as string;
-export function validVersion(ver: string) {
+export function isValidVersion(ver: string) {
     return semver.valid(semver.coerce(ver)) !== null;
 }
 
 export function parseVersion(ver: string) {
-    if (!validVersion(ver)) throw new TypeError(`Invalid version: ${ver}`);
+    if (!isValidVersion(ver)) throw new TypeError(`Invalid version: ${ver}`);
 
     const [major, minor, patch] = `${semver.coerce(ver)}`?.split('.') ?? [];
     return { major: parseInt(major), minor: parseInt(minor), patch: parseInt(patch) };
@@ -15,8 +15,8 @@ export function parseVersion(ver: string) {
 export function isSupportedVersion(versionRange: string, supportedVersion?: string) {
     supportedVersion = supportedVersion || version;
 
-    if (!validVersion(versionRange)) throw new TypeError(`Invalid version: ${versionRange}`);
-    if (!validVersion(supportedVersion)) throw new TypeError(`Invalid supported version: ${supportedVersion}`);
+    if (!isValidVersion(versionRange)) throw new TypeError(`Invalid version: ${versionRange}`);
+    if (!isValidVersion(supportedVersion)) throw new TypeError(`Invalid supported version: ${supportedVersion}`);
 
     return semver.satisfies(supportedVersion, versionRange);
 }
