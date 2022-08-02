fa72140745117f8739247f4e0c731ad2c8ce160e
GhexterCortes <ghextershumies@gmail.com>
2022-08-02 22:21:11 +0800

    rename `CommandHaltReasonBase`

--- src/reciple/types/commands.ts
+++ src/reciple/types/commands.ts
@@ -9,7 +9,7 @@ import { CommandBuilderType, CommandExecuteData } from '../types/builders';
  */
 export type CommandHaltData<T extends CommandBuilderType = CommandBuilderType> = CommandErrorData<T>|CommandCooldownData<T>|(T extends CommandBuilderType.SlashCommand ? never : CommandInvalidArguments<T>|CommandMissingArguments<T>)|CommandMissingMemberPermissions<T>|CommandMissingBotPermissions<T>;
 
-export interface CommandHaltBaseData<T extends CommandBuilderType> {
+export interface CommandHaltReasonBase<T extends CommandBuilderType> {
     executeData: T extends CommandBuilderType.SlashCommand
                     ? SlashCommandExecuteData
                     : T extends CommandBuilderType.MessageCommand 
@@ -17,22 +17,22 @@ export interface CommandHaltBaseData<T extends CommandBuilderType> {
                         : CommandExecuteData
 }
 
-export interface CommandErrorData<T extends CommandBuilderType> extends CommandHaltBaseData<T> {
+export interface CommandErrorData<T extends CommandBuilderType> extends CommandHaltReasonBase<T> {
     reason: CommandHaltReason.Error; error: any;
 }
-export interface CommandCooldownData<T extends CommandBuilderType> extends CommandHaltBaseData<T>,CooledDownUser {
+export interface CommandCooldownData<T extends CommandBuilderType> extends CommandHaltReasonBase<T>,CooledDownUser {
     reason: CommandHaltReason.Cooldown;
 }
-export interface CommandInvalidArguments<T extends CommandBuilderType> extends CommandHaltBaseData<T> {
+export interface CommandInvalidArguments<T extends CommandBuilderType> extends CommandHaltReasonBase<T> {
     reason: CommandHaltReason.InvalidArguments; invalidArguments: MessageCommandOptionManager;
 }
-export interface CommandMissingArguments<T extends CommandBuilderType> extends CommandHaltBaseData<T> {
+export interface CommandMissingArguments<T extends CommandBuilderType> extends CommandHaltReasonBase<T> {
     reason: CommandHaltReason.MissingArguments; missingArguments: MessageCommandOptionManager;
 }
-export interface CommandMissingMemberPermissions<T extends CommandBuilderType> extends CommandHaltBaseData<T> {
+export interface CommandMissingMemberPermissions<T extends CommandBuilderType> extends CommandHaltReasonBase<T> {
     reason: CommandHaltReason.MissingMemberPermissions;
 }
-export interface CommandMissingBotPermissions<T extends CommandBuilderType> extends CommandHaltBaseData<T> {
+export interface CommandMissingBotPermissions<T extends CommandBuilderType> extends CommandHaltReasonBase<T> {
     reason: CommandHaltReason.MissingBotPermissions;
 }
 
