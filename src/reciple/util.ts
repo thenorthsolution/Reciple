f00f5c925fe700f52df069f6e7baaab2cfdaeb25
GhexterCortes <ghextershumies@gmail.com>
2022-08-28 15:32:16 +0800

    hhehehhehe stackoverflow

--- src/reciple/util.ts
+++ src/reciple/util.ts
@@ -1,7 +1,7 @@
 export function isClass<T extends any>(object: any): object is T  {
-    const isCtorClass = object.constructor && object.constructor.toString().substring(0, 5) === 'class';
-    if(object.prototype === undefined) return isCtorClass;
+    const isClassConstructor = object.constructor && object.constructor.toString().substring(0, 5) === 'class';
+    if(object.prototype === undefined) return isClassConstructor;
 
-    const isPrototypeCtorClass = object.prototype.constructor && object.prototype.constructor.toString && object.prototype.constructor.toString().substring(0, 5) === 'class';
-    return isCtorClass || isPrototypeCtorClass;
+    const isPrototypeClassConstructor = object.prototype.constructor && object.prototype.constructor.toString && object.prototype.constructor.toString().substring(0, 5) === 'class';
+    return isClassConstructor || isPrototypeClassConstructor;
 }
