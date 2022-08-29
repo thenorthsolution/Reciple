bfeb889adb6b13d35306fb340ccf9b14cbdfe63b
GhexterCortes <ghextershumies@gmail.com>
2022-08-29 13:46:52 +0800

    Revert ":-:"
    This reverts commit ebac5b3df2192fe650ac352c10f44b855d27e336.

--- tsconfigs/package.json.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-
-fs.writeFileSync('bin/cjs/package.json', JSON.stringify({ type: "commonjs" }, null, 2));
-fs.writeFileSync('bin/mjs/package.json', JSON.stringify({ type: "module" }, null, 2));
