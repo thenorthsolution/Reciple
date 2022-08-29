79cc903cd0afe12da7b098269dbe1481c760b126
GhexterCortes <ghextershumies@gmail.com>
2022-08-29 13:46:35 +0800

    Revert "idk regex :-:"
    This reverts commit 45cc1e6d6e1d851edd57b0ce55d27d07df9fe513.

--- tsconfigs/package.json.js
+++ tsconfigs/package.json.js
@@ -20,8 +20,8 @@ function fixImports(dir) {
 
         const contents = fs.readFileSync(file, 'utf-8');
         
-        let newContents = contents.replace(/(import .* from\s+['"])(.?.\/.*)(?=['"])/g, '$1$2.js');
-            newContents = newContents.replace(/(export .* from\s+['"])(.?.\/.*)(?=['"])/g, '$1$2.js');
+        let newContents = contents.replace(/(import .* from\s+['"])(.*)(?=['"])/g, '$1$2.js');
+            newContents = newContents.replace(/(export .* from\s+['"])(.*)(?=['"])/g, '$1$2.js');
 
         fs.writeFileSync(file, newContents);
         console.log(file);
