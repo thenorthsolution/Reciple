6d774bc068f4769a898a0509d9622f26ecc89334
GhexterCortes <ghextershumies@gmail.com>
2022-08-29 13:46:47 +0800

    Revert "fix esm imports"
    This reverts commit c23d3a694242fdc116f6232dba7a8d23dd9a74d7.

--- tsconfigs/package.json.js
+++ tsconfigs/package.json.js
@@ -1,27 +1,4 @@
-// @ts-check
 const fs = require('fs');
-const path = require('path');
 
 fs.writeFileSync('bin/cjs/package.json', JSON.stringify({ type: "commonjs" }, null, 2));
 fs.writeFileSync('bin/mjs/package.json', JSON.stringify({ type: "module" }, null, 2));
-
-console.log(`Fixing esm imports`);
-
-fixImports(path.join(process.cwd(), 'bin/mjs'));
-
-function fixImports(dir) {
-    const files = fs.readdirSync(dir).map(p => path.join(dir, p));
-
-    for (const file of files) {
-        if (fs.lstatSync(file).isDirectory()) {
-            fixImports(file);
-            continue;
-        }
-
-        const contents = fs.readFileSync(file, 'utf-8');
-        const newContents = contents.replace(/(import .* from\s+['"])(.*)(?=['"])/g, '$1$2.js');
-
-        fs.writeFileSync(file, newContents);
-        console.log(file);
-    }
-}
