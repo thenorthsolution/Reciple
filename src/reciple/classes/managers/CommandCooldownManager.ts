23592ddc55974c767155ac3c2f44f3c90db8058b
codefactor-io <support@codefactor.io>
2022-10-28 09:53:32 +0000

    [CodeFactor] Apply fixes

--- src/reciple/classes/managers/CommandCooldownManager.ts
+++ src/reciple/classes/managers/CommandCooldownManager.ts
@@ -57,7 +57,7 @@ export class CommandCooldownManager extends Array<CooledDownUser> {
         if (!Object.keys(options).length) throw new TypeError('Provide atleast one option to remove cooldown data.');
 
         const removed: CooledDownUser[] = [];
-        let i = 0;
+        const i = 0;
 
         for (let i = 0; i < this.length; i++) {
             if (!CommandCooldownManager.checkOptions(options, this[i])) continue;
