2b8ceef8a136bfc15d1a6ba0635b688bc5a11cbf
Ghex <ghextershumies@gmail.com>
2023-02-01 21:55:36 +0800

    test module

--- test/modules/module.js
+++ test/modules/module.js
@@ -1,28 +1,86 @@
-const { ContextMenuCommandBuilder } = require('reciple');
+const { CommandHaltReason, ContextMenuCommandBuilder, RecipleClient, version } = require('reciple');
 const { ApplicationCommandType } = require('discord.js');
 
 module.exports = {
-    versions: `^7`,
+    versions: `^${version}`,
     commands: [
+        new ContextMenuCommandBuilder()
+            .setName('Test')
+            .setType(ApplicationCommandType.User)
+            .setCooldown(10000)
+            .setExecute(async ({interaction}) => interaction.reply(`Context menu`))
+            .setHalt(
+                /**
+                 * 
+                 * @param {import('../../packages/reciple/src').ContextMenuCommandHaltData} data 
+                 */
+                async data => data.reason === CommandHaltReason.Cooldown ? data.executeData.interaction.reply(`Cooldown ends at ${data.cooldownData.endsAt}`) : undefined),
         {
             commandType: 2,
-            name: 'tae',
-            description: 'Tae ko',
-            execute: async ({message}) => message.reply('tite')
+            name: 'test',
+            description: 'Testing description',
+            validateOptions: true,
+            options: [
+                {
+                    name: 'option1',
+                    description: 'A required option',
+                    required: true
+                },
+                {
+                    name: 'option2',
+                    description: 'Optional option'
+                },
+                {
+                    name: 'option3',
+                    description: 'Optional validated',
+                    validator: value => value === '3',
+                }
+            ],
+            /**
+             * 
+             * @param {import('reciple').MessageCommandExecuteData} param0 
+             * @returns 
+             */
+            execute: async ({message, options}) => {
+                if (options.getOption('option1') === 'error') throw new Error('An example error');
+
+                await message.reply('Test');
+            },
+
+            /**
+             * @param {import('../../packages/reciple/src').MessageCommandHaltData} param0
+             */
+            halt: async ({ reason, executeData }) => {
+                if (reason !== CommandHaltReason.Error) return;
+
+                await executeData.message.reply(`An error occured!`);
+                return true;
+            }
         },
         {
             commandType: 3,
             name: 'test',
-            description: 'Sus',
-            execute: async ({interaction}) => interaction.reply('e')
-        },
-        new ContextMenuCommandBuilder()
-            .setName('Kill')
-            .setType(ApplicationCommandType.User)
-            .setExecute(async ({interaction}) => interaction.reply(`Killed this mf`))
+            description: 'A test command',
+            execute: async ({interaction}) => interaction.reply('Test!')
+        }
     ],
-    onLoad() {
-        console.log(`Loaded UwUue;`);
+    /**
+     * 
+     * @param {RecipleClient} client 
+     * @returns 
+     */
+    onLoad(client) {
+        client.on('recipleCommandHalt', data => client.logger?.log(`Command halt ${data.executeData.builder.name}: `, data));
+
+        client.logger.warn(`Started module!`);
         return true;
+    },
+
+    /**
+     * 
+     * @param {RecipleClient} client 
+     */
+    onStart(client) {
+        client.logger.warn(`Loaded module!`);
     }
 };
