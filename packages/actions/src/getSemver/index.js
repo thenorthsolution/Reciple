04ea504476d03524e3fbd2cce52b80ae9ecd66a1
Ghex <ghextershumies@gmail.com>
2023-02-05 14:13:43 +0800

    try changes

---
+++ packages/actions/src/getSemver/index.js
@@ -0,0 +1,18 @@
+import { getInput, setOutput } from '@actions/core';
+import { readFileSync } from 'fs';
+import { globby } from 'globby';
+import path from 'path';
+import { formatTag } from '../formatTag/formatTag.js';
+
+const pkg = getInput('package', { required: true });
+const packages = globby(`../../../*/package.json`, { cwd: process.cwd(), onlyFiles: true }).map(p => path.resolve(p));
+
+const pkgJson = packages.map(pkgJsonFile => JSON.parse(readFileSync(pkgJsonFile, 'utf-8'))).map(content => {
+    if (!content?.name || !content?.version) return null;
+    return formatTag(`${content.name}@${content.version}`);
+}).find(p => p?.package == pkg);
+
+if (pkgJson) {
+    setOutput('package', pkgJson.package);
+    setOutput('semver', pkgJson.semver);
+}
