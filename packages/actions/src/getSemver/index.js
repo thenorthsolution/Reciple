29aadfd4c438fb99bd8808ebda796dd32abdd42e
Ghex <ghextershumies@gmail.com>
2023-02-05 14:28:35 +0800

    checkit

--- packages/actions/src/getSemver/index.js
+++ packages/actions/src/getSemver/index.js
@@ -9,10 +9,14 @@ import { formatTag } from '../formatTag/formatTag.js';
 const pkg = getInput('package', { required: true });
 const packages = (await globby(`../../../*/package.json`, { cwd: process.cwd(), onlyFiles: true })).map(p => path.resolve(p));
 
+console.log(`All packages:\n`, packages.join('\n'));
+
 const pkgJson = packages.map(pkgJsonFile => JSON.parse(readFileSync(pkgJsonFile, 'utf-8'))).map(content => {
     if (!content?.name || !content?.version) return null;
     return formatTag(`${content.name}@${content.version}`);
 }).find(p => p?.package == pkg);
+
+console.log(`Output:\n`, pkgJson);
 
 if (pkgJson) {
     setOutput('package', pkgJson.package);
