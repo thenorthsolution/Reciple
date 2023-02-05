12c34db43a2cda9229d7248987c5a69163fc5659
Ghex <ghextershumies@gmail.com>
2023-02-05 22:43:58 +0800

    cache docs ig

---
+++ packages/reciple/src/exports.ts
@@ -0,0 +1,4 @@
+export * from './classes/Config';
+export * from './utils/cli';
+export * from './utils/logger';
+export * from './utils/modules';
--- packages/reciple/src/index.ts
+++ packages/reciple/src/index.ts
@@ -1,5 +1,2 @@
 export * from '@reciple/client';
-export * from './classes/Config';
-export * from './utils/cli';
-export * from './utils/logger';
-export * from './utils/modules';
+export * from './exports';
