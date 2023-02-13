057590afbec74fd17b71cf95813ecd36b999d016
Ghex <ghextershumies@gmail.com>
2023-02-13 09:32:42 +0800

    update

--- packages/create-reciple/src/create.ts
+++ packages/create-reciple/src/create.ts
@@ -1,5 +1,5 @@
 import { ChildProcess, spawn } from 'child_process';
-import { copyFileSync, existsSync, lstatSync, mkdirSync, readdirSync, statSync } from 'fs';
+import { copyFileSync, existsSync, lstatSync, mkdirSync, readFileSync, readdirSync, statSync, writeFileSync } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -12,6 +12,15 @@ export async function create(cwd: string, templateDir: string): Promise<void> {
 
     copyFile(templateDir, cwd);
     copyFile(path.join(root, 'assets'), cwd, f => f.replace('dot.', '.'));
+
+    const { devDependencies } = JSON.parse(readFileSync(path.join(root, 'package.json'), 'utf-8'));
+
+    let packageJSON = readFileSync(path.join(cwd, 'package.json'), 'utf-8');
+
+    packageJSON = packageJSON.replace('RECIPLE', devDependencies.reciple);
+    packageJSON = packageJSON.replace('DISCORDJS', devDependencies['discord.js']);
+
+    writeFileSync(path.join(cwd, 'package.json'), packageJSON);
 
     await runScript('npm', cwd, ['install']);
     await runScript('npx', cwd, ['reciple', '-y']);
