8753eab1d03885b3b1be3026703ccec707feaf95
Ghex <ghextershumies@gmail.com>
2023-02-14 11:57:01 +0800

    fix

--- packages/reciple/src/utils/modules.ts
+++ packages/reciple/src/utils/modules.ts
@@ -2,6 +2,7 @@ import { Awaitable, path } from 'fallout-utility';
 import { IConfig } from '../classes/Config';
 import { cwd } from './cli';
 import { existsSync, lstatSync, mkdirSync, readdirSync } from 'fs';
+import micromatch from 'micromatch';
 
 export async function getModules(config: IConfig['modules'], filter?: (filename: string) => Awaitable<boolean>): Promise<string[]> {
     const modules: string[] = [];
@@ -11,24 +12,26 @@ export async function getModules(config: IConfig['modules'], filter?: (filename:
         const dir = path.isAbsolute(folder) ? folder : path.join(cwd, folder);
 
         if (isDynamicPattern(dir)) {
+            let modulesFolders = await globby(dir, {
+                    cwd,
+                    onlyDirectories: true,
+                    absolute: true
+                });
+
+                modulesFolders = modulesFolders.filter(f => !micromatch.isMatch(path.basename(f), config.exclude));
 
             modules.push(...await getModules({
                 ...config,
-                modulesFolders: await globby(dir, {
-                    cwd,
-                    gitignore: true,
-                    ignore: config.exclude.map(p => path.isAbsolute(p) ? p : path.join(cwd, p)),
-                    onlyDirectories: true,
-                    absolute: true
-                })
+                modulesFolders
             }));
+
             continue;
         }
 
         if (!existsSync(dir)) mkdirSync(dir, { recursive: true });
         if (!lstatSync(dir).isDirectory()) continue;
 
-        const files = readdirSync(dir).map(file => path.join(dir, file));
+        const files = readdirSync(dir).map(file => path.join(dir, file)).filter(f => !micromatch.isMatch(path.basename(f), config.exclude));
         modules.push(...files.filter(file => (filter ? filter(file) : file.endsWith('.js'))));
     }
 
