a565e0b79e72c0fb159ec109a19e4b11ef539d0a
Ghex <ghextershumies@gmail.com>
2023-02-14 19:00:15 +0800

    publish bitc-

--- packages/reciple/src/utils/cli.ts
+++ packages/reciple/src/utils/cli.ts
@@ -17,5 +17,6 @@ export const command = new Command()
     .option('--env', '.env file location')
     .parse();
 
+
 export const flags = command.opts();
 export const cwd = command.args[0] ? path.resolve(command.args[0]) : process.cwd();
