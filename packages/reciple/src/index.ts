b5c73d1e602a4d8de6e17405a213ef2b5977d005
Ghex <ghextershumies@gmail.com>
2023-02-26 19:39:30 +0800

    config

--- packages/reciple/src/index.ts
+++ packages/reciple/src/index.ts
@@ -1,9 +1,9 @@
 import { IConfig } from './classes/Config';
-import { RecipleClient as Client } from '@reciple/client';
+import { RecipleClient as Client, RecipleClientOptions } from '@reciple/client';
 
 export * from '@reciple/client';
 export * from './exports';
 
 export declare class RecipleClient<Ready extends boolean = boolean> extends Client<Ready> {
-    config: IConfig;
+    config: RecipleClientOptions["recipleOptions"] & Partial<IConfig>;
 }
