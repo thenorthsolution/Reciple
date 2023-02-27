ae118b6b0cf6cefeb62d2d7f11d0af16171f00dc
Ghex <ghextershumies@gmail.com>
2023-02-27 09:50:30 +0800

    some message fixes

--- test/modules/test.ts
+++ test/modules/test.ts
@@ -1,9 +1,15 @@
-import { RecipleModuleScript, realVersion } from 'reciple';
+import { ApplicationCommandType } from 'discord.js';
+import { ContextMenuCommandBuilder } from 'reciple';
 
 export default {
-    versions: [`^${realVersion}`],
-    commands: [],
+    versions: [`^7`],
+    commands: [
+        new ContextMenuCommandBuilder()
+            .setName('test')
+            .setType(ApplicationCommandType.Message)
+            .setExecute(async ({ interaction }) => { await interaction.reply(`UwU`); })
+    ],
     onStart() {
         return true;
     }
-} satisfies RecipleModuleScript;
+};
