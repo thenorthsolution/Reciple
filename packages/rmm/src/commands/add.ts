c89fc3a6b0b60cdac5a8046ceb12d786667ffce4
Ghex <ghextershumies@gmail.com>
2023-03-04 10:26:32 +0800

    remove rmm

--- packages/rmm/src/commands/add.ts
+++
@@ -1,8 +0,0 @@
-import { Command } from 'commander';
-import { Registry, cli } from '../index.js';
-
-export default (repository: Registry) => cli
-    .command('add')
-    .aliases(['i', 'install'])
-    .description('Install or add modules')
-    .action(async (args, command: Command) => {})
--- packages/rmm/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './classes/base/FileReader.js';
-export * from './classes/Registry.js';
-export * from './utils/cli.js';
