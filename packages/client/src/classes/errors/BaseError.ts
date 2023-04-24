cefd86df4443708faacea73151d74232bf7867a7
Ghex <69035887+NotGhex@users.noreply.github.com>
2023-04-24 12:46:53 +0800

    mommy

--- packages/client/src/classes/errors/BaseError.ts
+++ packages/client/src/classes/errors/BaseError.ts
@@ -5,7 +5,7 @@ import { replacePlaceholders } from '../../utils/functions';
 import kleur from 'kleur';
 
 export interface BaseErrorCodes {
-    [code: string|number]: string;
+    [code: string|number]: (() => string)|string;
 }
 
 export enum ErrorType {
@@ -18,7 +18,9 @@ export abstract class BaseError<ErrorCodes extends BaseErrorCodes = BaseErrorCod
     abstract readonly errorType: ErrorType;
 
     constructor(readonly code: keyof BaseErrorCodes, errorCodes: ErrorCodes, ...placeholders: RestOrArray<string>) {
-        super(`${kleur.red().bold(code)} ${replacePlaceholders(errorCodes[code], ...normalizeArray(placeholders))}`);
+        const message = errorCodes[code];
+
+        super(`${kleur.red().bold(code)} ${replacePlaceholders(typeof message === 'function' ? message() : message, ...normalizeArray(placeholders))}`);
     }
 
     public isCommandError(): this is CommandError {
--- packages/client/src/classes/errors/ModuleError.ts
+++ packages/client/src/classes/errors/ModuleError.ts
@@ -3,13 +3,13 @@ import { BaseError, ErrorType } from './BaseError';
 import kleur from 'kleur';
 
 export const ModuleErrorCodes = {
-    'LoadModuleFail': `Failed to load module ${kleur.blue('%0%')}:\n  ${kleur.red('%1%')}`,
-    'UnsupportedModule': `module ${kleur.blue('%0%')} does not support Reciple ${kleur.green('%1%')}`,
-    'InvalidScript': 'Invalid Reciple module script',
-    'NoSupportedVersions': `Module script is missing the required ${kleur.blue('(versions: string|string[])')} property`,
-    'InvalidOnStartEvent': `Module script's ${kleur.blue('onStart(): boolean|Promise<boolean>')} method is not a valid function`,
-    'InvalidOnLoadEvent': `Module script's ${kleur.blue('(onLoad(): void|Promise<void>)')} method is not a valid function`,
-    'InvalidOnUnloadEvent': `Module script's ${kleur.blue('(onUnload(): void|Promise<void>)')} method is not a valid function`
+    'LoadModuleFail': () => `Failed to load module ${kleur.blue('%0%')}:\n  ${kleur.red('%1%')}`,
+    'UnsupportedModule': () => `module ${kleur.blue('%0%')} does not support Reciple ${kleur.green('%1%')}`,
+    'InvalidScript': () => 'Invalid Reciple module script',
+    'NoSupportedVersions': () => `Module script is missing the required ${kleur.blue('(versions: string|string[])')} property`,
+    'InvalidOnStartEvent': () => `Module script's ${kleur.blue('onStart(): boolean|Promise<boolean>')} method is not a valid function`,
+    'InvalidOnLoadEvent': () => `Module script's ${kleur.blue('(onLoad(): void|Promise<void>)')} method is not a valid function`,
+    'InvalidOnUnloadEvent': () => `Module script's ${kleur.blue('(onUnload(): void|Promise<void>)')} method is not a valid function`
 }
 
 export type ModuleErrorCodes = typeof ModuleErrorCodes;
