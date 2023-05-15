41306135681991f07589e02d18ebec606c1d7d50
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-05-15 20:20:22 +0800

    yes

--- example/modules/npmLoader/npmLoader.js
+++ example/modules/npmLoader/npmLoader.js
@@ -13,12 +13,15 @@ export class NPMLoader extends RecipleNPMLoader {
     async onStart(client) {
         // Change the node_modules path
         this.nodeModulesFolder = path.join(this.cwd, 'node_modules');
+
         // Use config value of disableVersionChecks
         this.disableVersionChecks = !!client.config.modules?.disableModuleVersionCheck;
+
         // Ignored packages
         this.ignoredPackages = [];
+
         // Define to only use modules that are in package.json dependencies and dev dependencies
-        this.packageJsonPath = path.join(this.cwd, 'package.json');
+        // this.packageJsonPath = path.join(this.cwd, 'package.json');
 
         return super.onStart(client);
     }
