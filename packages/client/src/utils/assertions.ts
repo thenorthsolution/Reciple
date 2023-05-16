6069588e353669e5ecb6266c51688480e29d4f3e
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-05-16 13:00:33 +0800

    separate functions to better trace the error cause

--- packages/client/src/utils/assertions.ts
+++ packages/client/src/utils/assertions.ts
@@ -2,12 +2,32 @@ import { s } from '@sapphire/shapeshift';
 import { RecipleModuleScript } from '../classes/RecipleModule';
 import { anyCommandBuilderPredicate, anyCommandDataPredicate, stringOrArrayOfStringPredicate } from './predicates';
 
-export function validateModuleScript(moduleScript: unknown): asserts moduleScript is RecipleModuleScript {
-    const script = moduleScript as RecipleModuleScript;
+export function validateModuleScriptVersions(versions: unknown): asserts versions is RecipleModuleScript['versions'] {
+    stringOrArrayOfStringPredicate.parse(versions);
+}
 
-    stringOrArrayOfStringPredicate.parse(script?.versions);
-    s.union(anyCommandBuilderPredicate, anyCommandDataPredicate).array.optional.parse(script?.commands);
-    s.instance(Function).parse(script?.onStart);
-    s.instance(Function).optional.parse(script?.onLoad);
-    s.instance(Function).optional.parse(script?.onUnload);
+export function validateModuleScriptCommands(commands: unknown): asserts commands is RecipleModuleScript['commands'] {
+    s.union(anyCommandBuilderPredicate, anyCommandDataPredicate).array.optional.parse(commands);
+}
+
+export function validateModuleOnStart(onStart: unknown): asserts onStart is RecipleModuleScript['onLoad'] {
+    s.instance(Function).parse(onStart);
+}
+
+export function validateModuleOnLoad(onLoad: unknown): asserts onLoad is RecipleModuleScript['onLoad'] {
+    s.instance(Function).optional.parse(onLoad);
+}
+
+export function validateModuleOnUnload(onUnload: unknown): asserts onUnload is RecipleModuleScript['onLoad'] {
+    s.instance(Function).optional.parse(onUnload);
+}
+
+export function validateModuleScript(moduleScript: unknown): asserts moduleScript is RecipleModuleScript {
+    const script = moduleScript as Partial<RecipleModuleScript>;
+
+    validateModuleScriptVersions(script?.versions)
+    validateModuleScriptCommands(script.commands);
+    validateModuleOnStart(script.onStart);
+    validateModuleOnLoad(script?.onLoad);
+    validateModuleOnUnload(script?.onUnload);
 }
