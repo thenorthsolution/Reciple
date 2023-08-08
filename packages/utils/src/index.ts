f705492e8ca21d92430e94f13a6d03074955f801
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-08-08 18:03:39 +0800

    fix

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,5 @@
 export * from './helpers/parseMessageURL';
 export * from './helpers/recursiveDefaults';
 export * from './helpers/replacePlaceholders';
+export * from './helpers/resolveFromCachedCollection';
 export * from './helpers/resolvePackageManager';
