bd7305c254eb9b0007ae073c974833186dec3462
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-09-23 09:59:04 +0800

    lol why did I make this

--- packages/utils/src/helpers/resolveFromCachedCollection.ts
+++ packages/utils/src/helpers/resolveFromCachedCollection.ts
@@ -4,6 +4,7 @@ import { Collection } from '@discordjs/collection';
  * Resolve item from cache or fetch if not cached
  * @param id Item id
  * @param manager Item manager
+ * @deprecated Just use fetch with force set to false
  */
 export async function resolveFromCachedCollection<V>(id: string, manager: { cache: Collection<string, V>; fetch(key: string): Promise<V|null> }): Promise<V> {
     const data = manager.cache.get(id) ?? await manager.fetch(id);
