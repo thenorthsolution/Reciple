a9cea2b1f4daa64b6a07ac1ece9a53cc2df53239
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-10-05 17:48:05 +0800

    fix

--- packages/reciple/src/index.ts
+++ packages/reciple/src/index.ts
@@ -1,11 +1,9 @@
 import { RecipleClient as Client } from '@reciple/core';
 import { RecipleConfig } from './classes/Config';
-import { Logger } from 'fallout-utility';
 
 export * from '@reciple/core';
 export * from './exports';
 
 export declare class RecipleClient<Ready extends boolean = boolean> extends Client<Ready> {
     readonly config: RecipleConfig;
-    readonly logger?: Logger;
 }
