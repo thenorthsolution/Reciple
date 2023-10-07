4c0ab901dd437aa5bf92a586ea64fc019b744731
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-10-07 16:21:06 +0800

    lol

--- packages/core/src/classes/preconditions/CommandPermissionsPrecondition.ts
+++ packages/core/src/classes/preconditions/CommandPermissionsPrecondition.ts
@@ -19,7 +19,7 @@ export class CommandPermissionsPrecondition extends CommandPrecondition {
         const allowBot = data.builder.allow_bot;
         const dmPermission = data.builder.dm_permission;
 
-        const triggerData: CommandPreconditionTriggerData<MessageCommandExecuteData, { type: CommandPermissionsPreconditionTriggerDataType; }> = {
+        const triggerData: CommandPermissionsPreconditionTriggerData = {
             executeData: data,
             precondition,
             successful: false
@@ -39,10 +39,13 @@ export class CommandPermissionsPrecondition extends CommandPrecondition {
             return triggerData;
         }
 
-        if (data.message.inGuild() && (requiredBotPermissions || requiredMemberPermissions)) {
-            if (requiredBotPermissions && !CommandPermissionsPrecondition.userHasPermissionsIn(data.message.channel, requiredBotPermissions)) {
+        if (data.message.inGuild()) {
+            if (requiredBotPermissions && !(await CommandPermissionsPrecondition.userHasPermissionsIn(data.message.channel, requiredBotPermissions))) {
                 triggerData.message = `Bot doesn't have enough permissions to execute this command in this channel`;
-                triggerData.data = { type: CommandPermissionsPreconditionTriggerDataType.ClientNotEnoughPermissions };
+                triggerData.data = {
+                    type: CommandPermissionsPreconditionTriggerDataType.ClientNotEnoughPermissions,
+                    requiredPermissions: await CommandPermissionsPrecondition.getMissingPermissionsIn(data.message.channel, requiredBotPermissions)
+                };
 
                 return triggerData;
             }
@@ -52,7 +55,10 @@ export class CommandPermissionsPrecondition extends CommandPrecondition {
 
                 if (!member.permissionsIn(data.message.channel).has(requiredMemberPermissions)) {
                     triggerData.message = `User doesn't have enough permissions to execute this command in this channel`;
-                    triggerData.data = { type: CommandPermissionsPreconditionTriggerDataType.MemberNotEnoughPermissions };
+                    triggerData.data = {
+                        type: CommandPermissionsPreconditionTriggerDataType.MemberNotEnoughPermissions,
+                        requiredPermissions: await CommandPermissionsPrecondition.getMissingPermissionsIn(data.message.channel, requiredMemberPermissions)
+                    };
 
                     return triggerData;
                 }
@@ -74,7 +80,30 @@ export class CommandPermissionsPrecondition extends CommandPrecondition {
         return !!permissions?.has(userPermissions);
     }
 
+    public static async getMissingPermissionsIn(guildOrChannel: Guild|GuildBasedChannel, requiredPermissions: PermissionResolvable): Promise<PermissionsBitField> {
+        let permissions: PermissionsBitField|null = null;
+
+        if (guildOrChannel instanceof Guild) {
+            permissions = (await guildOrChannel.members.fetchMe()).permissions;
+        } else {
+            permissions = guildOrChannel.permissionsFor(guildOrChannel.client.user.id);
+        }
+
+        return new PermissionsBitField(permissions?.missing(requiredPermissions));
+    }
+
+    public static isCommandPermissionsPreconditionTriggerData(data: unknown): data is CommandPermissionsPreconditionTriggerData {
+        return ((data as CommandPermissionsPreconditionTriggerData).precondition instanceof CommandPermissionsPrecondition);
+    }
+
     public static create(): CommandPermissionsPrecondition {
         return new CommandPermissionsPrecondition(CommandPermissionsPrecondition.data);
     }
 }
+
+export interface CommandPermissionsPreconditionNotEnoughPermissionsTriggerData {
+    type: CommandPermissionsPreconditionTriggerDataType.ClientNotEnoughPermissions|CommandPermissionsPreconditionTriggerDataType.MemberNotEnoughPermissions;
+    requiredPermissions: PermissionsBitField;
+}
+
+export type CommandPermissionsPreconditionTriggerData = CommandPreconditionTriggerData<MessageCommandExecuteData, { type: Exclude<CommandPermissionsPreconditionTriggerDataType, CommandPermissionsPreconditionNotEnoughPermissionsTriggerData['type']>; }|CommandPermissionsPreconditionNotEnoughPermissionsTriggerData>;
