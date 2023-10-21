4363a5dbcc6311f00dd4432d16f1d308c0432894
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-10-21 11:14:41 +0800

    things

---
+++ packages/utils/src/helpers/mentions.ts
@@ -0,0 +1,30 @@
+import { Client, FormattingPatterns, GuildMember, GuildResolvable, Message, User, UserMention, UserResolvable } from 'discord.js';
+
+export interface FetchMentionOrIdOptions {
+    client: Client;
+    user: UserResolvable|UserMention;
+    guild?: GuildResolvable;
+    force?: boolean;
+}
+
+export function getMentionId(user: UserResolvable|UserMention): string|null {
+    const id = typeof user === 'string'
+        ? user.match(FormattingPatterns.User)?.[1] ?? null
+        : user instanceof Message
+            ? user.author.id
+            : user.id;
+
+    return !id && typeof user === 'string' ? user : id;
+}
+
+export async function fetchMentionOrId(options: Omit<FetchMentionOrIdOptions, 'guild'>): Promise<User>;
+export async function fetchMentionOrId(options: FetchMentionOrIdOptions): Promise<GuildMember>;
+export async function fetchMentionOrId(options: FetchMentionOrIdOptions): Promise<User|GuildMember> {
+    const id = getMentionId(options.user);
+    if (!id) throw new Error(`Invalid user resolvable ${id}`);
+
+    if (!options.guild) return options.client.users.fetch(id, { force: options.force });
+
+    const guild = await options.client.guilds.fetch({ guild: options.guild, force: options.force });
+    return guild.members.fetch({ user: id, force: options.force });
+}
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,8 +1,11 @@
 export * from './classes/MessageURLData';
-export * from './helpers/getDirModuleType';
-export * from './helpers/messageURLData';
+
+export * from './helpers/modules';
+export * from './helpers/mentions';
+export * from './helpers/messageURL';
 export * from './helpers/recursiveDefaults';
 export * from './helpers/replacePlaceholders';
 export * from './helpers/resolveEnvProtocol';
 export * from './helpers/resolvePackageManager';
+
 export * from './constants';
