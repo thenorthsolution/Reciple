200eb60d2b6cfb327e34e44500cda3c83376079d
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-10-22 21:17:25 +0800

    don't use experimental

--- example/modules/loader/npmLoader.js
+++ example/modules/loader/npmLoader.js
@@ -2,6 +2,5 @@ import { RecipleNPMLoader } from '@reciple/npm-loader';
 import path from 'path';
 
 export default new RecipleNPMLoader({
-    nodeModulesFolder: path.join(process.cwd(), '../node_modules'),
-    foldersPerWorker: 229
+    nodeModulesFolder: path.join(process.cwd(), '../node_modules')
 });
