5db3eb207a9cde999e4ca3da7bb12e8a962baef0
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-10-22 20:42:10 +0800

    real

--- example/modules/loader/npmLoader.js
+++ example/modules/loader/npmLoader.js
@@ -2,5 +2,6 @@ import { RecipleNPMLoader } from '@reciple/npm-loader';
 import path from 'path';
 
 export default new RecipleNPMLoader({
-    nodeModulesFolder: path.join(process.cwd(), '../node_modules')
+    nodeModulesFolder: path.join(process.cwd(), '../node_modules'),
+    foldersPerWorker: 200
 });
