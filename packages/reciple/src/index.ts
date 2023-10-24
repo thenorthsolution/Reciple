a14e10814e8b439b50a9954dc1b548432e70cda5
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-10-24 13:44:21 +0800

    e

--- packages/reciple/src/index.ts
+++ packages/reciple/src/index.ts
@@ -4,6 +4,7 @@ import { RecipleConfig } from './classes/Config';
 export * from '@reciple/core';
 export * from './exports';
 
+// TODO: Fix types
 export declare class RecipleClient<Ready extends boolean = boolean> extends Client<Ready> {
     readonly config: RecipleConfig;
 }
