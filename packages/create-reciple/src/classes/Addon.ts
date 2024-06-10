d772f2548ee3294f6dae0419e456bf622bbdee96
Cat++ <69035887+NotGhex@users.noreply.github.com>
2024-06-10 11:43:21 +0800

    set tarball data

--- packages/create-reciple/src/classes/Addon.ts
+++ packages/create-reciple/src/classes/Addon.ts
@@ -80,7 +80,7 @@ export class Addon implements AddonOptions {
             }
         }
 
-        return {
+        return this.tarballData = {
             packageJson,
             initialModuleContent
         };
