9e9cfe221d891376debf1aa796ee62febaad997c
Cat++ <69035887+NotGhex@users.noreply.github.com>
2024-06-14 10:03:21 +0800

    export base

--- packages/message-command-utils/src/index.ts
+++ packages/message-command-utils/src/index.ts
@@ -4,5 +4,6 @@ export * from './classes/builders/MessageCommandIntegerOptionBuilder.js';
 export * from './classes/builders/MessageCommandNumberOptionBuilder.js';
 export * from './classes/builders/MessageCommandRoleOptionBuilder.js';
 export * from './classes/builders/MessageCommandUserOptionBuilder.js';
+export * from './classes/structures/BaseMessageCommandOptionBuilder.js';
 
 export * from './types/types.js';
