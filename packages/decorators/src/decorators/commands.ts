885f7a542b3d1fc8421b2b7e850a4e7eb33aa6db
Cat++ <69035887+NotGhex@users.noreply.github.com>
2024-07-13 22:56:03 +0800

    add example

--- packages/decorators/src/decorators/commands.ts
+++ packages/decorators/src/decorators/commands.ts
@@ -2,6 +2,26 @@ import { ContextMenuCommandBuilder, type ContextMenuCommandExecuteFunction, type
 import { recipleModuleMetadataSymbol } from '../types/constants.js';
 import type { RecipleModuleDecoratorMetadata } from '../types/structures.js';
 
+/**
+ * Sets a context menu command decorator.
+ * 
+ * ```
+ * ＠setRecipleModule()
+ * class MyModule implements RecipleModuleData {
+ *     ＠setRecipleModuleStart()
+ *     async onStart() {
+ *         return true;
+ *     }
+ *
+ *     ＠setContextMenuCommand({ name: 'Test', type: 'Message' })
+ *     async handleContextMenuExecute(data: ContextMenuCommandExecuteData) {
+ *         await data.interaction('Test');
+ *     }
+ * }
+ * ```
+ * @param data The data for the context menu command.
+ * @returns The decorator function.
+ */
 export function setContextMenuCommand<T extends ContextMenuCommandExecuteFunction>(data: Omit<ContextMenuCommandBuilder, 'execute'|'setExecute'>|Omit<ContextMenuCommandBuilderData, 'execute'|'command_type'>) {
     return function(target: { constructor: { prototype: { [recipleModuleMetadataSymbol]?: RecipleModuleDecoratorMetadata; }; }; }, propertyKey: string|symbol, descriptor?: TypedPropertyDescriptor<T>) {
         if (!descriptor) throw new Error(`@setContextMenuCommand must be used on a method`);
@@ -21,6 +41,27 @@ export function setContextMenuCommand<T extends ContextMenuCommandExecuteFunctio
     }
 }
 
+
+/**
+ * Sets a message command decorator.
+ * 
+ * ```ts
+ * ＠setRecipleModule()
+ * class MyModule implements RecipleModuleData {
+ *     ＠setRecipleModuleStart()
+ *     async onStart() {
+ *         return true;
+ *     }
+ *
+ *     ＠setMessageCommand({ name: 'test', description: 'test command', aliases: ['t'] })
+ *     async handleMessageCommandExecute(data: MessageCommandExecuteData) {
+ *         await data.message('Test');
+ *     }
+ * }
+ * ```
+ * @param data The data for the message command.
+ * @returns The decorator function.
+ */
 export function setMessageCommand<T extends MessageCommandExecuteFunction>(data: Omit<MessageCommandBuilder, 'execute'|'setExecute'>|Omit<MessageCommandBuilderData, 'execute'|'command_type'>) {
     return function(target: { constructor: { prototype: { [recipleModuleMetadataSymbol]?: RecipleModuleDecoratorMetadata; }; }; }, propertyKey: string|symbol, descriptor?: TypedPropertyDescriptor<T>) {
         if (!descriptor) throw new Error(`@setMessageCommand must be used on a method`);
@@ -40,6 +81,26 @@ export function setMessageCommand<T extends MessageCommandExecuteFunction>(data:
     }
 }
 
+/**
+ * Sets a slash command decorator.
+ * 
+ * ```
+ * ＠setRecipleModule()
+ * class MyModule implements RecipleModuleData {
+ *     ＠setRecipleModuleStart()
+ *     async onStart() {
+ *         return true;
+ *     }
+ *
+ *     ＠setSlashCommand({ name: 'Test', description: 'Test command' })
+ *     async handleSlashCommandExecute(data: SlashCommandExecuteData) {
+ *         await data.interaction('Test');
+ *     }
+ * }
+ * ```
+ * @param data The data for the slash command.
+ * @returns The decorator function.
+ */
 export function setSlashCommand<T extends SlashCommandExecuteFunction>(data: Omit<AnySlashCommandBuilder, 'execute'|'setExecute'>|Omit<SlashCommandBuilderData, 'execute'|'command_type'>) {
     return function(target: { constructor: { prototype: { [recipleModuleMetadataSymbol]?: RecipleModuleDecoratorMetadata; }; }; }, propertyKey: string|symbol, descriptor?: TypedPropertyDescriptor<T>) {
         if (!descriptor) throw new Error(`@setSlashCommand must be used on a method`);
